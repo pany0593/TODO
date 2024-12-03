2034c50a05743b844e53e6530fc0b564cfa0117f
pany0593 <1569987504@qq.com>
2024-12-03 15:07:12 +0800

    add: course foreign key userid

--- backend/schema.sql
+++ backend/schema.sql
@@ -10,8 +10,10 @@ CREATE TABLE users (
 -- 创建课程表
 CREATE TABLE course (
     course_id VARCHAR(50) PRIMARY KEY,      -- 课程ID，主键
+    user_id INT NOT NULL,                         -- 用户ID，外键
     course_name VARCHAR(100) NOT NULL,             -- 课程名称
     teacher_name VARCHAR(50) NOT NULL              -- 任课教师
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE, -- 关联用户表
 );
 
 -- 创建任务表
@@ -22,6 +24,5 @@ CREATE TABLE task (
     status INT NOT NULL,                          -- 任务状态：待完成0/已完成1
     deadline DATE NOT NULL,                        -- 截止日期
     description TEXT NOT NULL,                     -- 任务描述
-    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE, -- 关联用户表
     FOREIGN KEY (course_id) REFERENCES Course(course_id) ON DELETE CASCADE -- 关联课程表
 );
