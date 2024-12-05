8646ab80ac44b9eec380c3e6347a7bf1db5012d9
pany0593 <1569987504@qq.com>
2024-12-05 18:37:01 +0800

    add: TopBar and TopButton components

--- frontend/src/components/TopBar/TopBar.jsx
+++ frontend/src/components/TopBar/TopBar.jsx
@@ -1,7 +1,13 @@
+import TopButton from './TopButton/TopButton';
+import './TopBar.css';  // 引入 CSS 文件
+
 function TopBar() {
     return (
         <div className="top-bar">
-            <h2>My First React App</h2>
+            <h2>TODO备忘录</h2>
+            <div className="top-button">
+                <TopButton />
+            </div>
         </div>
     );
 }
---
+++ frontend/src/components/TopBar/TopButton/TopButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TopButton.css';  // 引入外部CSS文件
+
+function TopButton() {
+    return (
+        <button className="top-button">User</button>
+);
+}
+
+export default TopButton;
