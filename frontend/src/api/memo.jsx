ce53cd4f00cf7a6dda7b3dcfe6f1f8362cae12e5
pany0593 <1569987504@qq.com>
2024-12-06 21:04:08 +0800

    Refactor add_memo API to match new data structure

--- frontend/src/api/memo.jsx
+++ frontend/src/api/memo.jsx
@@ -4,9 +4,10 @@ import {apiRequest} from "./api.jsx";
 // 注册请求函数
 export const add_memo = async (course, start, end, title, description) => {
     const data = {
-        course_name: "test",
-        status: 0,
-        deadline: end,
+        course: course,
+        start: start,
+        end: end,
+        title: title,
         description: description
     };
     return await apiRequest('http://127.0.0.1:5000/api/add_memo', 'post', data, true);
