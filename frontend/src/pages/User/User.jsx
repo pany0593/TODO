c3ef0357e8d98c709012b5a7e5af75ba01b244cd
pany0593 <1569987504@qq.com>
2024-12-06 13:55:48 +0800

    Remove unused Link import in User page

--- frontend/src/pages/User/User.jsx
+++ frontend/src/pages/User/User.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"; // 导入 useNavigate
+import { useNavigate } from "react-router-dom"; // 导入 useNavigate
 import "./User.css";
 
 function User() {
