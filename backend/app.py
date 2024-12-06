a7dd169d7a9dd97ba70afa8200962eb7f397c94a
pany0593 <1569987504@qq.com>
2024-12-06 14:20:39 +0800

    Add CORS support to Flask app for cross-origin requests

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 from backend.controller import bp, jwt
 from backend.utils import config
+from flask_cors import CORS
 
 """
 创建并配置 Flask 应用
@@ -12,6 +13,7 @@ app.config['JWT_SECRET_KEY'] = config['app']['secret_key']
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=7)
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
 jwt = jwt.init_app(app)
+CORS(app)  # 启用跨域支持
 # 注册路由
 app.register_blueprint(bp, url_prefix='/api')
 
